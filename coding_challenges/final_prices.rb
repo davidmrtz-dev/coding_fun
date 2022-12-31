8b947bef54eec922ca52865071eab5cf4c70cbbc
David Mtz <davidelimg1@gmail.com>
2022-12-31 16:13:35 -0600

    Fixed lints, updated rubocop config.

--- coding_challenges/final_prices.rb
+++ coding_challenges/final_prices.rb
@@ -1,13 +1,13 @@
 require 'byebug'
-require "ostruct"
+require 'ostruct'
 module Implementation
   def final_prices(prices)
-    ans_list = prices.each_with_index.map do |price, idx|
+    ans_list = prices.each_with_index.map do |_price, idx|
       OpenStruct.new({ value: 0, discount_applied: false, final_price: 0, idx: idx })
     end
 
     prices.each_with_index do |price, idx|
-      if ans_list.any?{|v| v.value >= price && v.discount_applied == false}
+      if ans_list.any? { |v| v.value >= price && v.discount_applied == false }
         idxs = ans_list.select do |e|
           e.value >= price && e.discount_applied == false
         end.map(&:idx)
