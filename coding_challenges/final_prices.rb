5fbf6baeafdb12a7bf93eab0b28f4a334bad3e56
David Mtz <davidelimg1@gmail.com>
2022-12-31 16:30:57 -0600

    Removed unused var.

--- coding_challenges/final_prices.rb
+++ coding_challenges/final_prices.rb
@@ -3,7 +3,7 @@ require 'ostruct'
 module Implementation
   def final_prices(prices)
     ans_list = prices.each_with_index.map do |_price, idx|
-      OpenStruct.new({ value: 0, discount_applied: false, final_price: 0, idx: idx })
+      OpenStruct.new({ value: 0, discount_applied: false, idx: idx })
     end
 
     prices.each_with_index do |price, idx|
