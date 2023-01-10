35a98f9db4b5e173968e23ea28cc8f81341e1a2f
David Mtz <davidelimg1@gmail.com>
2023-01-10 08:50:22 -0600

    [CHORE]: Added solution, debugging.

--- coding_challenges/longest_substring_without_repeated_chars.rb
+++ coding_challenges/longest_substring_without_repeated_chars.rb
@@ -1,8 +1,23 @@
 require 'byebug'
 require 'ostruct'
+require 'set'
 
 module Implementation
   def length_of_longest_substring(s)
-    s
+    se = Set.new()
+    left = 0
+    ans = 0
+
+    (0..(s.length)).each do |idx|
+      byebug
+      if se.include?(s[idx])
+        se.delete(s[left])
+        left += 1
+      end
+      se.add(s[idx])
+      ans = [ans, idx - left + 1].max
+    end
+
+    ans
   end
 end
--- main.rb
+++ main.rb
@@ -5,3 +5,5 @@ require_relative './coding_challenges/longest_substring_without_repeated_chars'
 include Implementation
 
 puts length_of_longest_substring('abcabcbb')
+puts length_of_longest_substring('bbbbb')
+puts length_of_longest_substring('pwwkew')
