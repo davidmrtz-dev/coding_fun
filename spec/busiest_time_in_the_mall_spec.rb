eb41007c506df8280de333c95f02dde766df899a
David Mtz <davidelimg1@gmail.com>
2023-01-12 09:26:03 -0600

    [WIP]: Updated spec for busiest time functionality

--- spec/busiest_time_in_the_mall_spec.rb
+++ spec/busiest_time_in_the_mall_spec.rb
@@ -2,7 +2,16 @@ require 'spec_helper'
 
 RSpec.describe 'Buestiest time in the mall' do
   it 'should assert' do
-    expect(1)
-      .to eq 1
+    expect(Implementation::find_busiest_period([
+      [1487799425, 14, 1],
+      [1487799425, 4,  0],
+      [1487799425, 2,  0],
+      [1487800378, 10, 1],
+      [1487801478, 18, 0],
+      [1487801478, 18, 1],
+      [1487901013, 1,  0],
+      [1487901211, 7,  1],
+      [1487901211, 7,  0]
+    ])).to eq 1487800378
   end
 end
