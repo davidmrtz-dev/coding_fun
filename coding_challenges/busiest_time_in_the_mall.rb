f3a12e5565dacd0cf6bbc1d315698664cc5d7ee4
David Mtz <davidelimg1@gmail.com>
2023-01-12 08:13:39 -0600

    [WIP]: Busiest time in mall resolution, added method to module

---
+++ coding_challenges/busiest_time_in_the_mall.rb
@@ -0,0 +1,8 @@
+require 'byebug'
+require 'ostruct'
+
+module Implementation
+  def find_busiest_period(arr)
+    arr
+  end
+end
--- main.rb
+++ main.rb
@@ -2,6 +2,7 @@ require_relative './coding_challenges/n_repeated_in_size_2n'
 require_relative './coding_challenges/final_prices'
 require_relative './coding_challenges/longest_substring_without_repeated_chars'
 require_relative './coding_challenges/fizz_buzz'
+require_relative './coding_challenges/busiest_time_in_the_mall'
 
 include Implementation
 
@@ -9,4 +10,14 @@ include Implementation
 # puts length_of_longest_substring('bbbbb')
 # puts length_of_longest_substring('pwwkew')
 
-fizz_buzz(15)
+puts find_busiest_period([
+  [1487799425, 14, 1],
+  [1487799425, 4,  0],
+  [1487799425, 2,  0],
+  [1487800378, 10, 1],
+  [1487801478, 18, 0],
+  [1487801478, 18, 1],
+  [1487901013, 1,  0],
+  [1487901211, 7,  1],
+  [1487901211, 7,  0]
+])
