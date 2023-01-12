958ea1f1d5e6a630219cd4cc63cfb4426d5414c3
David Mtz <davidelimg1@gmail.com>
2023-01-12 09:22:58 -0600

    [WIP]: Added functionality, incresing testing coverage

--- coding_challenges/busiest_time_in_the_mall.rb
+++ coding_challenges/busiest_time_in_the_mall.rb
@@ -3,6 +3,27 @@ require 'ostruct'
 
 module Implementation
   def find_busiest_period(arr)
-    arr
+    last_max_ts = 0
+    last_max_ppl = 0
+    current_ts = 0
+    current_ppl = 0
+
+    arr.each do |entry|
+      ts, ppl, action = entry
+      # byebug
+      if ts != current_ts
+        if current_ppl > last_max_ppl
+          last_max_ppl = current_ppl
+          last_max_ts = current_ts
+        end
+        current_ppl = 0
+        current_ts = ts
+      end
+
+      current_ppl += ppl if action.eql? 1
+      current_ppl -= ppl if action.eql? 0
+    end
+
+    last_max_ts
   end
 end
