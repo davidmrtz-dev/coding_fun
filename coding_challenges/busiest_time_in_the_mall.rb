5b0707b2e6af5ea90523096d434af7d3be5a981f
David Mtz <davidelimg1@gmail.com>
2023-01-13 11:14:47 -0600

    [CHORE]: Updating behavior to pass tests

--- coding_challenges/busiest_time_in_the_mall.rb
+++ coding_challenges/busiest_time_in_the_mall.rb
@@ -5,23 +5,30 @@ module Implementation
   def find_busiest_period(arr)
     last_max_ts = 0
     last_max_ppl = 0
-    current_ts = 0
-    current_ppl = 0
+
+    last_ts = 0
+    total_ppl = 0
+
+    return arr.first.first if arr.length.eql? 1
 
     arr.each do |entry|
+      byebug
       ts, ppl, action = entry
-      # byebug
-      if ts != current_ts
-        if current_ppl > last_max_ppl
-          last_max_ppl = current_ppl
-          last_max_ts = current_ts
-        end
-        current_ppl = 0
-        current_ts = ts
-      end
 
-      current_ppl += ppl if action.eql? 1
-      current_ppl -= ppl if action.eql? 0
+      total_ppl += ppl if action.eql? 1
+      total_ppl -= ppl if action.eql? 0
+
+      if ts != last_ts
+        if total_ppl > last_max_ppl
+          last_max_ppl = total_ppl
+          last_max_ts = ts
+        end
+
+        # total_ppl = 0
+        last_ts = ts
+      else
+        last_max_ppl = total_ppl
+      end
     end
 
     last_max_ts
