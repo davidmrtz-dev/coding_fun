d5d5f5cd545e511a543dee7f4c2b398f213b574a
David Mtz <davidelimg1@gmail.com>
2023-01-26 17:02:35 -0600

    [CHORE]: Init challenge.

---
+++ coding_challenges/medium/sparse_arrays.rb
@@ -0,0 +1,5 @@
+module Implementation
+  def matching_strings(strings, queries)
+    puts strings, queries
+  end
+end
--- main.rb
+++ main.rb
@@ -1,10 +1,7 @@
-require_relative './coding_challenges/easy/plus_minus'
+require_relative './coding_challenges/medium/sparse_arrays'
 
 require 'byebug'
 
 include Implementation
 
-plus_minus([-4, 3, -9, 0, 4, 1])
-# 0.500000
-# 0.333333
-# 0.166667
+matching_strings('ok', 'ko')
