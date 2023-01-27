fc133e4720db018d4fef0d963c537e10616a54fd
David Mtz <davidelimg1@gmail.com>
2023-01-26 18:27:57 -0600

    [FEAT]: Solved CC

--- coding_challenges/medium/sparse_arrays.rb
+++ coding_challenges/medium/sparse_arrays.rb
@@ -2,11 +2,16 @@ module Implementation
   def matching_strings(strings, queries)
     query_h = Hash.new
     queries.each { |q| query_h[q] = 0}
+    result = Array.new(queries.length)
 
     strings.each do |str|
       query_h[str] = query_h[str] + 1 unless query_h[str].nil?
     end
 
-    query_h.values
+    queries.each_with_index do |q, idx|
+      result[idx] = query_h[q] ? query_h[q] : 0
+    end
+
+    result
   end
 end
