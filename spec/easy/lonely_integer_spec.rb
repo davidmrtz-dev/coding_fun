769cd1dfb7f74c0e34dbda704ec874bb3a88e1dd
David Mtz <davidelimg1@gmail.com>
2023-01-27 11:54:34 -0600

    [WIP]: Passing tests

--- spec/easy/lonely_integer_spec.rb
+++ spec/easy/lonely_integer_spec.rb
@@ -4,4 +4,8 @@ RSpec.describe 'LonelyInteger' do
   it 'should return the element that is not repeated' do
     expect(Implementation::lonely_integer([1, 2, 3, 4, 3, 2, 1])).to eq 4
   end
+
+  it 'should return the element that is not repeated' do
+    expect(Implementation::lonely_integer([4, 9, 95, 93, 57, 4, 57, 93, 9])).to eq 4
+  end
 end
