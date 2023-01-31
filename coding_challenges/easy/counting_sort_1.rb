f1d4d8e94e4449b317ad4537d127409a7c109f24
David Mtz <davidelimg1@gmail.com>
2023-01-31 16:45:40 -0600

    [FEAT]: Solved challenge.

--- coding_challenges/easy/counting_sort_1.rb
+++ coding_challenges/easy/counting_sort_1.rb
@@ -1,5 +1,9 @@
 module Implementation
   def counting_sort(arr)
-    'hello world'
+    ans = Array.new(100, 0)
+    arr.each do |num|
+      ans[num] += 1 if num < 100
+    end
+    ans
   end
 end
