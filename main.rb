b47be71caff6406f7e4a83895d146e86ac3e9c86
David Mtz <davidelimg1@gmail.com>
2023-01-31 16:26:04 -0600

    [FEAT]: Implemented func.

--- main.rb
+++ main.rb
@@ -1,11 +1,7 @@
-require_relative './coding_challenges/easy/diagonal_difference'
+require_relative './coding_challenges/easy/counting_sort_1'
 
 require 'byebug'
 
 include Implementation
 
-puts diagonal_difference([
-	[11, 2, 4],
-	[4, 5, 6],
-	[10, 8, -12]
-])
+puts counting_sort('ok')
