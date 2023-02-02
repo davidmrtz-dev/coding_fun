8cbeb9f1ff6e3e782070c2093e24882e52103ba9
David Mtz <davidelimg1@gmail.com>
2023-02-01 20:37:58 -0600

    [WIP]: Not solved yet, moving to next one.

--- coding_challenges/medium/flipping_matrix.rb
+++ coding_challenges/medium/flipping_matrix.rb
@@ -1,5 +1,25 @@
 module Implementation
   def flipping_matrix(matrix)
-    matrix
+    mid = matrix.length / 2
+    ene = mid - 1
+    sum = 0
+
+    for idx in 0..mid
+      for idj in 0..mid
+        puts matrix[idx][idj]
+        puts matrix[idx][ene - idj]
+        puts matrix[ene - idx][idj]
+        puts matrix[ene - idx][ene - idj]
+        puts '--------------------------'
+        temp = Array.new
+        temp << matrix[idx][idj]
+        temp << matrix[idx][ene - idj]
+        temp << matrix[ene - idx][idj]
+        temp << matrix[ene - idx][ene - idj]
+        sum += temp.max
+      end
+    end
+
+    sum
   end
 end
