b18f98fd75d3cc0dc423a45d38b8d89e1d7d32dd
David Mtz <davidelimg1@gmail.com>
2023-02-01 19:38:05 -0600

    [CHORE]: Added function.

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
-require_relative './coding_challenges/easy/permuting_two_arrays'
+require_relative './coding_challenges/medium/flipping_matrix'
 
 require 'byebug'
 
 include Implementation
 
-puts two_arrays(10, [2, 1, 3], [7, 8, 9])
+puts flipping_matrix('hello matrix')
