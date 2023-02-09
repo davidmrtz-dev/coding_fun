ce3aed1d84cb909675dfd909170abe52ac6c30d6
David Mtz <davidelimg1@gmail.com>
2023-02-08 19:26:35 -0600

    [FEAT]: Solved

--- coding_challenges/medium/hotel_reservation.rb
+++ coding_challenges/medium/hotel_reservation.rb
@@ -1,6 +1,41 @@
+require 'byebug'
+
 def check_capacity(hotel, people)
-  puts ['hello', hotel, people]
+  # byebug
+  hours = []
+  current_start = nil
+
+  (hotel[:open]...hotel[:close]).each do |start_hour|
+    # byebug
+    end_hour = start_hour + 1
+    total_people = 0
+
+    hotel[:entries].each do |entry|
+      # byebug
+      if (start_hour >= entry[:from] && start_hour < entry[:to]) || (end_hour > entry[:from] && end_hour <= entry[:to])
+        total_people += entry[:people]
+      end
+    end
+
+    # byebug
+    if total_people + people <= hotel[:capacity]
+      if current_start.nil?
+        current_start = start_hour
+      end
+    else
+      if current_start
+        hours << [current_start, end_hour - 1]
+        current_start = nil
+      end
+    end
+  end
+
+  hours << [current_start, hotel[:close] - 1] if current_start
+
+  hours
 end
+
+
 
 hotel = {
   open: 9,
@@ -35,4 +70,4 @@ hotel = {
   ]
 }
 
-check_capacity(hotel, 1)
+print check_capacity(hotel, 1)
