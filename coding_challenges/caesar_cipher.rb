ced87e4591c65b92c46b6f501cb40a816d0f0e83
davidmrtz-dev <davidelimg1@gmail.com>
2023-03-29 21:14:02 -0600

    [FEAT]: Implemented solution for challenge.

--- coding_challenges/caesar_cipher.rb
+++ coding_challenges/caesar_cipher.rb
@@ -1,5 +1,23 @@
 module Implementation
   def caesar_cipher(s, k)
-    puts 'okok'
+    alpha = 'abcdefghijklmnopqrstuvwxyz'.split('')
+    enc = ''
+
+    s.each_char do |char|
+      unless char.match(/[a-zA-Z]/)
+        enc << char
+        next
+      end
+      is_cap = false
+      is_cap = true if char.match(/[A-Z]/)
+      char.downcase!
+      idx = alpha.find_index(char)
+      rotated_idx = (idx + k) % alpha.length
+      enc_letter = alpha[rotated_idx]
+      enc_letter = enc_letter.capitalize if is_cap
+      enc << enc_letter
+    end
+
+    enc
   end
 end
