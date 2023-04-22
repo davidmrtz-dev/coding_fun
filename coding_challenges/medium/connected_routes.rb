fee5615daee1649dad0216ece4ae48d0f02086e4
davidmrtz-dev <davidelimg1@gmail.com>
2023-04-21 23:37:22 -0600

    [FEAT]: Implemented functionality

--- coding_challenges/medium/connected_routes.rb
+++ coding_challenges/medium/connected_routes.rb
@@ -1,5 +1,20 @@
 module Implementation
   def connected_routes(routes)
-    'ok'
+    destinations = {}
+    routes.each do |origin, destination|
+      destinations[origin] = destination
+    end
+
+    start = (destinations.keys - destinations.values).first
+
+    connected_routes = [start]
+    while destinations[connected_routes.last]
+      connected_routes << destinations[connected_routes.last]
+    end
+
+    connected_routes
   end
 end
+
+
+{"SFO"=>"EWR", "SJC"=>"LAX", "DFW"=>"SJC", "EWR"=>"OAK", "LAX"=>"SFO"}
