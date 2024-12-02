583a6246b29284fe8b802b5490a86edf7541a2d8
davidmrtz-dev <davidelimg1@gmail.com>
2024-12-01 21:39:02 -0600

    [FEAT]: Solved CC

--- leetcode/programming_skills/easy/merge_strings_alternately.rb
+++ leetcode/programming_skills/easy/merge_strings_alternately.rb
@@ -1,19 +1,19 @@
 require 'byebug'
 
 def merge_alternately(word1, word2)
-  merged_array = []
+  result = []
   i = 0
 
   while i < word1.length || i < word2.length
-    merged_array << word1[i] if i < word1.length
-    merged_array << word2[i] if i < word2.length
+    result << word1[i] if i < word1.length
+    result << word2[i] if i < word2.length
     i += 1
   end
 
-  merged_array.join
+  result.join
 end
 
 puts merge_alternately('abc', 'pqr')
 
-# Time Complexity O(n)
-# Space Complexity O(n + m)
+# Time Complexity ?
+# Space Complexity ?
